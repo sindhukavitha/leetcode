19124ccc489faf29fb0fb3fcbc1cb4466abef244
sindhukavitha <sindhuk.22cse@kongu.edu>
2025-01-07 13:20:49 +0530

    Time: 2098 ms (39.52%), Space: 40.4 MB (51.58%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num<=1)
+        return false;
+        int sum=0;
+        for(int i=1;i<num;i++)
+        {
+            if(num%i==0)
+            sum=sum+i;
+        }
+        return sum==num;
+    }
+}
