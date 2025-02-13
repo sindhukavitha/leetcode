69da01dac9a078f61ab0a67b19406c77f608dd87
sindhukavitha <sindhuk.22cse@kongu.edu>
2025-02-14 00:08:46 +0530

    Time: 3 ms (95.06%), Space: 40.7 MB (35.25%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long l=0,r=(long)Math.sqrt(c);
+        while(l<=r)
+        {
+            long cur=l*l+r*r;
+            if(cur>c)
+            {
+                r--;
+            }
+            else if(cur<c)
+            {
+                l++;
+            }
+            else
+            {
+                return true;
+            }
+        }
+      return false;  
+    }
+}
